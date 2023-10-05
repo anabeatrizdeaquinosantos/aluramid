1649dc94acf44a40d04e1b5b0a715318e64ae83b
Ana Beatriz de Aquino <103039796+anabeatrizdeaquinosantos@users.noreply.github.com>
2023-10-05 09:31:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-document.querySelector('.tecla_pom');
-
 function tocaSomPom(){
 document.querySelector('#som_tecla_pom').play('');
 }
-Document.querySelector('.tecla_pom').onclick = tocaSomPom ();
+document.querySelectorAll('.tecla')
+//document.querySelector('.tecla_pom').onclick = toca
