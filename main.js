97485dfb6d4ac8d0e504689664c7868f2c9a74a7
Ana Beatriz de Aquino <103039796+anabeatrizdeaquinosantos@users.noreply.github.com>
2023-11-09 09:36:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play('');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
 }
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla')
 //document.querySelector('.tecla_pom').onclick = toca
+listaDeTeclas{1}.onclick = tocaSomClap;
+let contador = 0;
+while(contador < 9){
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_clap');
+    }
+    contador =  contador + 1;
